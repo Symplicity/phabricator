c49b9eb3cb761ac564d5a8e615404522e8ed3f7e
Svemir Brkic <svemir.brkic@gmail.com>
2012-06-17 21:24:34 -0400

    added table for daily stats

--- resources/sql/patches/071.shine.sql
+++ resources/sql/patches/071.shine.sql
@@ -3,12 +3,26 @@ CREATE DATABASE IF NOT EXISTS phabricator_shine;
 CREATE TABLE IF NOT EXISTS phabricator_shine.shine_badge (
   `id` INT UNSIGNED NOT NULL auto_increment PRIMARY KEY,
   `title` VARCHAR(64) NOT NULL,
-  `userPHID` VARCHAR(64) CHARACTER SET latin1 COLLATE latin1_bin NOT NULL,
-  `dateEarned` INT UNSIGNED NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `userPHID` VARCHAR(64) COLLATE utf8_bin NOT NULL,
+  `dateEarned` INT UNSIGNED NOT NULL,
+  `tally` int(10) unsigned NOT NULL DEFAULT '0',
+  KEY `userPHID`(`userPHID`),
+  KEY `title`(`title`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 INSERT INTO phabricator_directory.directory_item
   (name, description, href, categoryID, sequence, dateCreated, dateModified)
 VALUES
   ("Shine", "Badges! Awards! Kittens!", "/shine/", 5, 600,
     UNIX_TIMESTAMP(), UNIX_TIMESTAMP());
+
+CREATE TABLE IF NOT EXISTS phabricator_shine.shine_stats (
+  `id` INT UNSIGNED NOT NULL auto_increment PRIMARY KEY,
+  `dateLogged` INT UNSIGNED NOT NULL,
+  `userPHID` VARCHAR(64) COLLATE utf8_bin NOT NULL,
+  `title` VARCHAR(64) NOT NULL,
+  `tally` int(10) unsigned NOT NULL DEFAULT '0',
+  KEY `dateLogged`(`dateLogged`),
+  KEY `userPHID`(`userPHID`),
+  KEY `title`(`title`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
